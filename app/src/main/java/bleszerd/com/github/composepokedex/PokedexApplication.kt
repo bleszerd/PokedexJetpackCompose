8baaa36af0f5181cc70bca7674e597671a2f6fc1
Vinícius Resende <alive2k@programmer.net>
2021-09-27 15:20:52 -0300

    Initial commit

---
+++ app/src/main/java/bleszerd/com/github/composepokedex/PokedexApplication.kt
@@ -0,0 +1,15 @@
+package bleszerd.com.github.composepokedex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class PokedexApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+
+}
